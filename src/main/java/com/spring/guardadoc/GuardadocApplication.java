25a1625878431e9a7ab7ffa980a6a25576ca0e73
Pedro_Henrick <Pedrwsxd@gmail.com>
2024-11-02 09:56:46 -0300

    Primeiro commit

---
+++ src/main/java/com/spring/guardadoc/GuardadocApplication.java
@@ -0,0 +1,13 @@
+package com.spring.guardadoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuardadocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuardadocApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring/guardadoc/GuardadocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.guardadoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuardadocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
